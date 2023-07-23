61bc5efdc5a61e61984c702f7582e3288442c5ea
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-07-23 16:11:18 -0300

    Aplicando migrations

--- RestApi/Program.cs
+++ RestApi/Program.cs
@@ -55,8 +55,10 @@ app.Run();
 
 public class Product
 {
+    public int Id { get; set; }
     public string Code { get; set; } = null!;
     public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
 }
 
 public static class ProductRepository
